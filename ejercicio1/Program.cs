46643fe139cb6d8b16196f6a11e9f5da169a246f
Carlos <70349232+Gatote@users.noreply.github.com>
2023-03-07 19:40:40 -0600

    Add files via upload
    Subiendo ejercicios ce C#

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Introduce un numero");
+Decimal num1 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Introduce otro numero");
+Decimal num2 = Convert.ToDecimal(Console.ReadLine());
+Decimal resta=Math.Abs(num1-num2);
+if(num1>num2){
+    Console.WriteLine("El resultado de la resta es: {0}",resta*2);
+}else{
+    Console.WriteLine("El resultado de la resta es: {0}",resta);
+}
---
+++ ejercicio2/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+for (int i = 1; i <= 99; i += 1)
+{
+    if (i%2!=0){
+        Console.WriteLine(i);
+    }
+}
