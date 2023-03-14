c15209a58b8d7c85e42235032f9da0966e0eb01b
Carlos <70349232+Gatote@users.noreply.github.com>
2023-03-13 20:44:51 -0600

    Agregando ejercicios 8 y 9

---
+++ ejercicio8/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Ingrese números separados por un espacio:");
+string input = Console.ReadLine();
+string separador=" ";
+string[] numbers = input.Split(separador);
+int[] array = new int[numbers.Length];
+for (int i = 0; i < numbers.Length; i++) {
+    array[i] = int.Parse(numbers[i]);
+}
+int total=array.Length;
+int total_ordenados=0;
+while (total_ordenados<array.Length-1){
+    for (int i = 0; i < array.Length; i++) {
+        if (i<array.Length-1){
+            if (array[i]>array[i+1]){
+                int ntemp=array[i];
+                array[i]=array[i+1];
+                array[i+1]=ntemp;
+                total_ordenados=0;
+            }
+            else{
+                total_ordenados=total_ordenados+1;
+            }
+        }
+    }
+}
+
+Console.WriteLine("\n{0}\n",String.Join(separador, array));
---
+++ ejercicio9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Ingrese números separados por un espacio:");
+        string input = Console.ReadLine();
+        string separador=" ";
+        string[] numbers = input.Split(separador);
+        int[] array = new int[numbers.Length];
+        for (int i = 0; i < numbers.Length; i++) {
+            array[i] = int.Parse(numbers[i]);
+        }
+        Array.Sort(array);
+        Console.WriteLine("Numero buscado:");
+        int n = int.Parse(Console.ReadLine());
+
+        int resultado = busqueda(array, n);
+        if (resultado == -1) {
+            Console.WriteLine("El número no está en el arreglo");
+        } else {
+            Console.WriteLine("El número está en la posición {0}", resultado);
+        }
+    }
+
+    static int busqueda(int[] array, int n) {
+        int izquierda = 0;
+        int derecha = array.Length - 1;
+        while (izquierda <= derecha) {
+            int medio = izquierda + (derecha - izquierda) / 2;
+            if (array[medio] == n) {
+                return medio;
+            } else if (array[medio] < n) {
+                izquierda = medio + 1;
+            } else {
+                derecha = medio - 1;
+            }
+        }
+        return -1; // número no encontrado en el arreglo
+    }
+
+}
